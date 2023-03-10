fc7b3edd6599db4337640155fe72891eabd01256
chaduneli91 <111608444+chaduneli91@users.noreply.github.com>
2023-03-10 23:41:54 +0400

    input update

--- src/app/Services/add.service.ts
+++ src/app/Services/add.service.ts
@@ -1,29 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 @Injectable({
   providedIn: 'root'
 })
 export class AddService {
-
+  readonly ROOT_URL = environment.apiUrl;
 
   constructor(private _http: HttpClient) { }
 
   AddTeg(data: any): Observable<any>{
-    return this._http.post('http://localhost:3000/add', data)
+    return this._http.post(`${this.ROOT_URL}/add`, data)
   };
 
   list(): Observable<any>{
-    return this._http.get('http://localhost:3000/add')
+    return this._http.get(`${this.ROOT_URL}/add`)
   };
 
   delete(listId: any): Observable<any> {
-    return this._http.delete(`http://localhost:3000/add/${listId}`)
+    return this._http.delete(`${this.ROOT_URL}/add/${listId}`)
 
   }
 
   editTeg(itemId: number, item:any ):Observable<any> {
-    return this._http.put(`http://localhost:3000/add/${itemId}`, item);
+    return this._http.put(`${this.ROOT_URL}/add/${itemId}`, item);
   }
 
 
